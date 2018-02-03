eeccd637d3b20cb6ef7e41f2623f9570b7073930
Daniel Kantor <github@daniel-kantor.com>
2018-02-03 09:25:40 +0000

    added readme generator (#3)

---
+++ Makefile
@@ -0,0 +1,2 @@
+README.md: ./readme_template.md ./src/tests.json
+	(cat readme_template.md; cat src/tests.json | jq '.[] | .[] | [.[0], "`" +(.[1] | join("`, `")) + "`"] | join("@")' -c -r | sed "s/|/\\\|/g; s/@/|/g") > $@
--- src/travis_test.js
+++ src/travis_test.js
@@ -1,5 +1,5 @@
-import { tests_jq_web, tests_node_jq } from './test_spec.js'
 import { test_with_jq_web } from './test_helpers.js'
+const { tests_jq_web, tests_node_jq } = require('json-loader!./tests.json')
 
 tests_jq_web.forEach(test_with_jq_web)
 tests_node_jq.forEach(test_with_jq_web)
