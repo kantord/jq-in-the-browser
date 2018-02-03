21b37d098611385474c8146642b02efc0a13112e
Dániel Kántor <git@daniel-kantor.com>
2018-02-03 11:51:56 +0100

    Updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
+var webpack = require('webpack');
+var path = require('path');
+var libraryName = 'jq-in-the-browser';
+var outputFile = libraryName + '.js';
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+
 module.exports = {
   entry: './src/jq.js',
-  target: 'node',
+  entry: __dirname + '/src/index.js',
+  devtool: 'source-map',
   output: {
-    filename: './lib/jq.js'
+    path: __dirname + '/lib',
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    umdNamedDefine: true
   },
+  target: 'node',
+  plugins: [new UglifyJsPlugin()],
   module: {
     loaders: [
       {
         test: /\.pegjs$/,
         loader: 'pegjs-loader?trace=false'
-      }
+      },
+      {
+        test: /(\.jsx|\.js)$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules|bower_components)/
+      },
     ]
   }
 };
